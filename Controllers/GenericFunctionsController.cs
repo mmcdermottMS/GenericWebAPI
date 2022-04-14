ecd760da910aedbb8c36aa0791504dda35e3979c
Mike McDermott <mmcdermott@microsoft.com>
2022-04-14 13:22:33 -0500

    Intitial Commit

---
+++ Controllers/GenericFunctionsController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GenericWebAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class GenericFunctionsController : Controller
+    {
+        private readonly ILogger<GenericFunctionsController> _logger;
+
+        public GenericFunctionsController(ILogger<GenericFunctionsController> logger)
+        {
+            _logger = logger;
+        }
+
+        [Route("FunctionA")]
+        [HttpGet]
+        public string GetFunctionA()
+        {
+            _logger.LogInformation("Function A Called");
+
+            return $"Function A Called at {DateTime.UtcNow} UTC";
+        }
+    }
+}
---
+++ Controllers/WeatherForecastController.cs
@@ -0,0 +1,35 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace GenericWebAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [Route("Current")]
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            _logger.LogInformation("Weather Checked");
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
